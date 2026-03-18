5ec064f33024dd1dcb50891c2638b4c6752decde
saccharin01 <chousik01@gmail.com>
2026-03-18 18:54:10 +0900

    hotfix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@ export default function Footer() {
   return (
     <footer>
       <div className="px-60 text-sm space-y-1 bg-[#f0eff0]">
-        <div>대표 : 홍길동</div>
-        <div>사업자번호 : 123-45-67890</div>
-        <div>주소 : 부산광역시 어딘가</div>
-        <div>Copyright © DDingodng</div>
+        <div>대표 : 한영빈</div>
+        <div>사업자번호 : 714-86-03513</div>
+        <div>주소 : 대전광역시 서구 갈마로 189, 2층 주식회사 띵동</div>
+        <div>Copyright © DDingodng Corp</div>
       </div>
     </footer>
   );
